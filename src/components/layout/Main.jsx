8d303a9eeafedcc78c86af1a1f64196e73f77a1b
Marcos Chacon <marcoschacon39@gmail.com>
2021-11-27 00:27:01 -0200

    pequena mudança

--- src/components/layout/Main.jsx
+++ src/components/layout/Main.jsx
@@ -2,11 +2,11 @@ import { Container, Box} from "@mui/material";
 export default function Main(props) {
     return (
         <main>
-            <Box pt={2} pb={2}>
-                <Container>
+            <Container>
+                <Box my={2}>
                     {props.children}
-                </Container>
-            </Box>
+                </Box>
+            </Container>
         </main>
     )
 }
